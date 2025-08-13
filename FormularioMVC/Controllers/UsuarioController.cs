dda8b720fde8c99665fc5233f6d9b9980b293178
gtapiero <giovanny.tapiero@pi.edu.co>
2025-08-13 16:31:27 -0500

    Agregar archivos de proyecto.

---
+++ FormularioMVC/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FormularioMVC.Models;
+
+namespace FormularioMVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aquí puedes guardar en una base de datos o procesar la información
+                ViewBag.Mensaje = "Usuario registrado correctamente.";
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+    }
+}
---
+++ FormularioMVC/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormularioMVC.Models
+{
+    public class Usuario
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Correo { get; set; }
+    }
+}
